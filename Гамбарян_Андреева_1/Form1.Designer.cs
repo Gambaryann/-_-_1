d9fa6e2befc2e13e2a3c3bd3003384f2f394a065
Gambaryan <hrachruby@gmail.com>
2023-01-23 18:11:58 +0300

    Добавьте файлы проекта.

---
+++ Гамбарян_Андреева_1/Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace Гамбарян_Андреева_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Button
+            // 
+            this.Button.BackColor = System.Drawing.Color.BlueViolet;
+            this.Button.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Button.Location = new System.Drawing.Point(295, 264);
+            this.Button.Name = "Button";
+            this.Button.Size = new System.Drawing.Size(156, 32);
+            this.Button.TabIndex = 0;
+            this.Button.Text = "Button";
+            this.Button.UseVisualStyleBackColor = false;
+            this.Button.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.Button);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(800, 600);
+            this.MinimumSize = new System.Drawing.Size(800, 600);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Button;
+    }
+}
+
